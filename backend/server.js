69b9fea3ced9dfcd8b318a4b61fa78efee4495e6
Jaime González <jgonzalez1@uc.cl>
2020-02-27 03:26:38 -0300

    Simplify websocket server

--- backend/server.js
+++ backend/server.js
@@ -1,72 +1,36 @@
-import WebSocket from 'ws';
-import axios from 'axios';
+import WebSocketServer from 'ws';
+import http from 'http';
+import { getCityInfo } from './api/index.js';
+import { getCoordinates } from './redis/index.js';
 
-const ws = new WebSocket('ws://localhost.com:3001/ws', {
-	perMessageDeflate: false
-});
+const server = http.createServer();
 
-const citiesInfo = [
-	{
-		name: 'Santiago (CL)',
-		latitude: '-33.445992',
-		longitude: '-70.667061'
-	},
-	{
-		name: 'Zurich (CH)',
-		latitude: '47.376888',
-		longitude: '8.538030'
-	},
-	{
-		name: 'Auckland (NZ)',
-		latitude: '-36.848461',
-		longitude: '174.763336'
-	},
-	{
-		name: 'Sydney (AU)',
-		latitude: '-33.868820',
-		longitude: '151.209290'
-	},
-	{
-		name: 'London (UK)',
-		latitude: '51.517576',
-		longitude: '-0.079786'
-	},
-	{
-		name: 'Georgia (USA)',
-		latitude: '33.756529',
-		longitude: '-84.400996'
-	}
-];
+const wss = new WebSocketServer.Server({ server: server });
 
-const darkskyKey = '6c782e1b4eff74b07389ce26b9753ee4';
+console.log('Websocket server created');
 
-ws.on('connection', function connection(ws) {
-	setInterval(() => {
-		const citiesResults = citiesInfo.map(cityInfo => {
-			const { name } = cityInfo;
+const namesOfCities = [];
 
-			axios
-				.get(
-					`https://api.darksky.net/forecast/${darkskyKey}/${cityInfo.latitude},${cityInfo.longitude}`
-				)
-				.then(function(response) {
-					if (Math.rand(0, 1) < 0.1)
-						throw new Error('How unfortunate! The API Request Failed');
+wss.on('connection', function(ws) {
+	const intervalId = setInterval(function() {
+		const apiResults = namesOfCities.map(async cityName => {
+			const { latitude, longitude } = await getCoordinates(cityName);
 
-					const { currently } = response;
-					const { time, temperature } = currently;
-					const currentDate = new Date(time);
-					const hour = `${currentDate.getHours()}:${currentDate.getMinutes()}`;
-					return { name, hour, temperature };
-				})
-				.catch(function(error) {
-					return { name, hour: 'No new data', temperature: 'No new data' };
-				})
-				.then(function() {
-					// always executed
-				});
+			return await getCityInfo(cityName, latitude, longitude);
 		});
-
-		ws.send(citiesResults);
+		ws.send(apiResults);
 	}, 10000);
+
+	console.log('Websocket connection opened');
+
+	ws.on('close', function() {
+		console.log('Websocket connection closed');
+		clearInterval(intervalId);
+	});
 });
+
+const port = process.env.PORT || 5000;
+
+server.listen(port);
+
+console.log('Http server listening on %d', port);
