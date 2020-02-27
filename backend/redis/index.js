80d92aa19b4b09460a3445cc4596276657d02043
Jaime González <jgonzalez1@uc.cl>
2020-02-27 03:25:31 -0300

    Create redis methods and client

---
+++ backend/redis/index.js
@@ -0,0 +1,18 @@
+import redisClient from './redisClient.js';
+
+export async function getCoordinates(cityName) {
+	try {
+		const coordinates = await redisClient.get(cityName);
+		return JSON.parse(coordinates);
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+export async function registerApiError(cityName) {
+	try {
+		return await redisClient.hset('api.errors', new Date(), cityName);
+	} catch (error) {
+		console.error(error);
+	}
+}
