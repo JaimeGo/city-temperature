71e8290367c529061bdc0a701b6803b4117db8d0
Jaime González <jgonzalez1@uc.cl>
2020-02-27 19:52:53 -0300

    Change darksky's key

--- backend/src/api/apiClient.js
+++ backend/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const darkskyKey = '6c782e1b4eff74b07389ce26b9753ee4';
+const darkskyKey = '39779faae4ec01076fb6f7fd99daba43';
 
 export default axios.create({
 	baseURL: `https://api.darksky.net/forecast/${darkskyKey}/`
