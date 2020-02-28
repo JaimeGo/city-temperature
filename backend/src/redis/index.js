ddadf95938cdf9059d97fc0ba3c7baeb5b402d32
Jaime González <jgonzalez1@uc.cl>
2020-02-28 03:44:57 -0300

    Add logs to redis methods

--- backend/src/redis/index.js
+++ backend/src/redis/index.js
@@ -2,6 +2,7 @@ import redisClient from './redisClient';
 
 export async function getCoordinates(cityName) {
 	try {
+		console.log('GETCOORDS');
 		const latitude = await redisClient.hget(cityName, 'latitude');
 		const longitude = await redisClient.hget(cityName, 'longitude');
 		return { latitude, longitude };
@@ -12,6 +13,7 @@ export async function getCoordinates(cityName) {
 
 export async function getHourAndTemperature(cityName) {
 	try {
+		console.log('GETHOUR');
 		const hour = await redisClient.hget(cityName, 'hour');
 		const temperature = await redisClient.hget(cityName, 'temperature');
 		return { hour, temperature };
@@ -22,6 +24,7 @@ export async function getHourAndTemperature(cityName) {
 
 export async function setHourAndTemperature(cityName, hour, temperature) {
 	try {
+		console.log('SETHOUR');
 		await redisClient.hset(cityName, 'hour', hour);
 		await redisClient.hset(cityName, 'temperature', temperature);
 	} catch (error) {
@@ -31,6 +34,7 @@ export async function setHourAndTemperature(cityName, hour, temperature) {
 
 export async function registerApiError(cityName) {
 	try {
+		console.log('REGISTERERROR');
 		return await redisClient.hset('api.errors', new Date(), cityName);
 	} catch (error) {
 		console.error(error);
