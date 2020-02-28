fe541eebe8306e415e4f8a312f915c1696fba21b
Jaime González <jgonzalez1@uc.cl>
2020-02-28 14:52:45 -0300

    Remove unused prop in app component

--- frontend/src/components/app/index.js
+++ frontend/src/components/app/index.js
@@ -6,42 +6,10 @@ import CityTable from "../city-table";
 import Footer from "../footer";
 
 const App = () => {
-  const dataOfCities = [
-    {
-      name: "Santiago (CL)",
-      degree: "22",
-      hour: "12:00"
-    },
-    {
-      name: "Zurich (CH)",
-      degree: "22",
-      hour: "12:00"
-    },
-    {
-      name: "Auckland (NZ)",
-      degree: "22",
-      hour: "12:00"
-    },
-    {
-      name: "Sydney (AU)",
-      degree: "22",
-      hour: "12:00"
-    },
-    {
-      name: "Londres (UK)",
-      degree: "22",
-      hour: "12:00"
-    },
-    {
-      name: "Georgia (USA)",
-      degree: "22",
-      hour: "12:00"
-    }
-  ];
   return (
     <div className="app">
       <Header />
-      <CityTable dataOfCities={dataOfCities} />
+      <CityTable />
       <Footer />
     </div>
   );
