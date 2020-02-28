5811a30328e9cd565cf067e0df6894952b4f41d1
Jaime González <jgonzalez1@uc.cl>
2020-02-27 23:58:33 -0300

    Add env variable

--- backend/src/api/apiClient.js
+++ backend/src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const darkskyKey = '34be6c91a7242250aa974b11862e87a0';
+const darkskyKey = process.env.DARKSKY_KEY;
 
 export default axios.create({
 	baseURL: `https://api.darksky.net/forecast/${darkskyKey}/`
